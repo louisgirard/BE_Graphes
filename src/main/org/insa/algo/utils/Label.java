ea8ce97eb4bf5335d0666962661311ee3e74a8a1
Louis Girard <louis.girard17@hotmail.com>
2019-04-07 16:29:54 +0200

    Ajout class Label

---
+++ src/main/org/insa/algo/utils/Label.java
@@ -0,0 +1,43 @@
+package org.insa.algo.utils;
+
+import org.insa.graph.Arc;
+import org.insa.graph.Node;
+
+public class Label {
+	private Node currentNode;	
+	private boolean mark;	
+	private float cost;	
+	private Arc father;
+	
+	public Label() {
+		this.currentNode = null;
+		this.mark = false;
+		this.cost = 100000000000000f;
+		this.father = null;		
+	}
+	
+	//getter
+	public float getCost() {
+		return this.cost;
+	}
+	public Node getCurrentNode(){
+		return this.currentNode;
+	}
+	public boolean getMark() {
+		return this.mark;
+	}
+	public Arc getFather() {
+		return this.father;
+	}
+	//setter
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	public void setCurrentNode(Node node) {
+		this.currentNode = node;
+	}
+	public void setMark() {
+		this.mark = true;
+	}
+	
+}
